5e4499f80fd521f574be9cd295f3e79ed464e363
Jason Allen <Jason Allen>
2016-04-15 11:44:18 -0600

    generator working

--- personalProject/js/mainCtrl.js
+++ personalProject/js/mainCtrl.js
@@ -1,4 +1,12 @@
 angular.module('nameMaker').controller("mainCtrl", function($scope, randomService){
   $scope.name = "Jason";
 
+  $scope.newName = randomService.newName;
+  $scope.nameList = randomService.nameList;
+  $scope.logTrack = randomService.logTrack;
+  $scope.randomName = function(letter, iter){
+    console.log("hit")
+    $scope.result = randomService.randomName(letter, iter);
+  }
+
 });
--- personalProject/js/myService.js
+++ personalProject/js/myService.js
@@ -1,26 +1,57 @@
 angular.module("nameMaker").service('randomService', function(){
 
-var alphabet = [["b", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"],
-   ["a", "e", "i", "o", "u", "y"]];
+  var alphabet = [["b", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"],
+     ["a", "e", "i", "o", "u", "y"]];
 
-this.selector = Math.floor(Math.random() * 2);
+  this.newName = [];
+  this.nameList = [];
+  this.logTrack = [];
 
-var newName = [];
-var nameList = [];
 
-  this.randomName = function(arr, num, iterate){
-        for(var j = 0; j < iterate; j++){
-          if(num !== NaN && iterate !== NaN) {
-            for(var i = 0; i < num; i++)
-            newName.push(arr[Math.floor(Math.random() * 26)]);
-            var finalName = newName.join('');
-            nameList.push(finalName);
-            newName = [];
+   this.randomName = function(num, iter){
+      if(!isNaN(num) && !isNaN(iter)) {
+        for(var j = 0; j < iter; j++){
+          for(var i = 0; i < num; i++) {
+            var aNum;
+            var selector;
+            if(this.logTrack[0] === 0 && this.logTrack[1] === 0){
+              selector = Math.floor(Math.random() * 2);
+              aNum = Math.floor(Math.random() * alphabet[1].length);
+              this.newName.push(alphabet[1][aNum]);
+              this.logTrack.push(1);
+              if(this.logTrack.length > 2){
+                this.logTrack.shift();
+              }
+            }
+            else if(this.logTrack[0] === 1 && this.logTrack[1] === 1){
+              selector = Math.floor(Math.random() * 2);
+              aNum = Math.floor(Math.random() * alphabet[0].length);
+              this.newName.push(alphabet[0][aNum]);
+              this.logTrack.push(0);
+              if(this.logTrack.length > 2){
+                this.logTrack.shift();
+              }
+            }
+            else {
+              selector = Math.floor(Math.random() * 2);
+              aNum = Math.floor(Math.random() * alphabet[selector].length);
+              this.newName.push(alphabet[selector][aNum]);
+              this.logTrack.push(selector);
+              if(this.logTrack.length > 2){
+                this.logTrack.shift();
+              }
+            }
           }
-          return nameList;
+
+          var rawName = this.newName.join('');
+          var finalName = rawName.charAt(0).toUpperCase() + rawName.substring(1);
+          finalName.charAt(0).toUpperCase();
+          this.nameList.push(finalName);
+          this.newName = [];
         }
-      }
-    });
+        console.log("done");
+      return this.nameList;
+    }
+  }
 
-
-//array length. That's what I need to remember.
+});
