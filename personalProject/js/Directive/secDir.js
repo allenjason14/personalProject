39f7d9bdb93edc14ccee69e044fafd038337b883
Jason Allen <Jason Allen>
2016-04-20 13:16:15 -0600

    second directive up

--- personalProject/js/Directive/secDir.js
+++ personalProject/js/Directive/secDir.js
@@ -1,7 +1,26 @@
 angular.module('nameMaker').directive('secDir', function(){
 
   return{
-    $('button').on('hover', function(){
-      $('this').addClass("active");
-    })  }
-})
+    restrict: "AE",
+    template: "<div class='a'>Hello</div>",
+    controller: function() {
+
+    $(document).ready(function(){
+
+      $('.a').hover(
+        function(){
+          console.log('onhover');
+        $(this).addClass('active');
+      },
+      function() {
+        $(this).removeClass('active');
+      })
+
+    });
+    }
+  }
+});
+
+// $( "p" ).on( "mouseover", function() {
+//   $( this ).css( "color", "red" );
+// });
